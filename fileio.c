c7c05de4fee96d2de1ed9987170644f335c7b082
Michelle Tang <mtang1@stuy.edu>
2018-10-23 18:56:53 -0400

    testing open/close

--- fileio.c
+++ fileio.c
@@ -1,7 +1,11 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(){
-  return 0
+int main() {
+	int file = open("zhou_garvey.txt", O_RDWR); // gives read and write access
+	write(file, "wow\n", 4);
+	close(file);
+  return 0;
 }
