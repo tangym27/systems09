6ab901f7e1b5db0a7a7331e77da9de1f72bcff8d
GarveyZhou <gzhou2@stuy.edu>
2018-10-23 19:33:39 -0400

    added read

--- fileio.c
+++ fileio.c
@@ -1,7 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 int main(){
-  return 0
+  char sent[20] = "Hi world";
+  char *p = calloc(75,sizeof(char));
+  int file = open("zhou_garvey.txt",O_RDWR);
+  int f =  read(file,p,75);
+  printf("reading file %s",p);
+  close(file);
+  return 0;
 }
